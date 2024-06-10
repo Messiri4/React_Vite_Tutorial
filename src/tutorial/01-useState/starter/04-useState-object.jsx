e63e8adbcaf48eab30a80783bb9cdace2ecbbe25
Messiri4 <messirimiracle@gmail.com>
2024-06-10 13:18:04 +0100

    usestate-gotcha

--- src/tutorial/01-useState/starter/04-useState-object.jsx
+++ src/tutorial/01-useState/starter/04-useState-object.jsx
@@ -1,7 +1,34 @@
-import { useState } from 'react';
+import { useState } from "react";
 
 const UseStateObject = () => {
-  return <h2>useState object example</h2>;
+  const [person, setPerson] = useState({
+    name: "Miracle",
+    age: 23,
+    hobby: "Coding",
+  });
+  // const [name, setName] = useState("Miracle");
+  // const [age, setAge] = useState(23);
+  // const [hobby, setHobby] = useState("Coding");
+
+  const displayPerson = () => {
+    // setPerson({
+    //   name: "Betty",
+    //   age: 42,
+    //   hobby: "Actress",
+    // });
+    setPerson({ ...person, name: "susan" });
+  };
+
+  return (
+    <div>
+      <h2>{person.name}</h2>
+      <h2>{person.age}</h2>
+      <h2>{person.hobby}</h2>
+      <button type="button" className="btn" onClick={displayPerson}>
+        Show Gemini
+      </button>
+    </div>
+  );
 };
 
 export default UseStateObject;
